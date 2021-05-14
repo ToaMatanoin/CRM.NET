8826b4ef4820e9780c4467ff71311a4d57bd6452
Alejandro Caceres <man2019mc@hotmail.com>
2021-05-14 10:17:02 -0600

    Completado formulario de Cliente

--- CRM/Presentacion/Clientes.Designer.vb
+++ CRM/Presentacion/Clientes.Designer.vb
@@ -23,12 +23,14 @@ Partial Class Clientes
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Txt_Buscar = New System.Windows.Forms.TextBox()
+        Me.Cmb_Buscar = New System.Windows.Forms.ComboBox()
+        Me.Lb_Buscar = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.BtnRegresar = New System.Windows.Forms.Button()
         Me.BtnModificar = New System.Windows.Forms.Button()
-        Me.BtnBuscar = New System.Windows.Forms.Button()
-        Me.BtnEliminar = New System.Windows.Forms.Button()
         Me.BtnIngresar = New System.Windows.Forms.Button()
+        Me.BtnEliminar = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.TxtIDCliente = New System.Windows.Forms.TextBox()
@@ -48,20 +50,23 @@ Partial Class Clientes
         Me.Label6 = New System.Windows.Forms.Label()
         Me.TxtNomEmpresa = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Dgv_Listado = New System.Windows.Forms.DataGridView()
+        Me.eliminar = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Chk_Eliminar = New System.Windows.Forms.CheckBox()
         Me.GroupBox1.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         Me.GroupBox4.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Dgv_Listado, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Txt_Buscar)
+        Me.GroupBox1.Controls.Add(Me.Cmb_Buscar)
+        Me.GroupBox1.Controls.Add(Me.Lb_Buscar)
+        Me.GroupBox1.Controls.Add(Me.BtnEliminar)
         Me.GroupBox1.Controls.Add(Me.GroupBox2)
         Me.GroupBox1.Controls.Add(Me.BtnModificar)
-        Me.GroupBox1.Controls.Add(Me.BtnBuscar)
-        Me.GroupBox1.Controls.Add(Me.BtnEliminar)
         Me.GroupBox1.Controls.Add(Me.BtnIngresar)
         Me.GroupBox1.Location = New System.Drawing.Point(16, 68)
         Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
@@ -71,10 +76,37 @@ Partial Class Clientes
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         '
+        'Txt_Buscar
+        '
+        Me.Txt_Buscar.Location = New System.Drawing.Point(551, 42)
+        Me.Txt_Buscar.Margin = New System.Windows.Forms.Padding(4)
+        Me.Txt_Buscar.Multiline = True
+        Me.Txt_Buscar.Name = "Txt_Buscar"
+        Me.Txt_Buscar.Size = New System.Drawing.Size(253, 52)
+        Me.Txt_Buscar.TabIndex = 12
+        '
+        'Cmb_Buscar
+        '
+        Me.Cmb_Buscar.FormattingEnabled = True
+        Me.Cmb_Buscar.Location = New System.Drawing.Point(608, 16)
+        Me.Cmb_Buscar.Margin = New System.Windows.Forms.Padding(4)
+        Me.Cmb_Buscar.Name = "Cmb_Buscar"
+        Me.Cmb_Buscar.Size = New System.Drawing.Size(196, 24)
+        Me.Cmb_Buscar.TabIndex = 11
+        '
+        'Lb_Buscar
+        '
+        Me.Lb_Buscar.AutoSize = True
+        Me.Lb_Buscar.Location = New System.Drawing.Point(548, 19)
+        Me.Lb_Buscar.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Lb_Buscar.Name = "Lb_Buscar"
+        Me.Lb_Buscar.Size = New System.Drawing.Size(52, 17)
+        Me.Lb_Buscar.TabIndex = 10
+        Me.Lb_Buscar.Text = "Buscar"
+        '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.BtnRegresar)
-        Me.GroupBox2.Location = New System.Drawing.Point(697, 0)
+        Me.GroupBox2.Location = New System.Drawing.Point(836, 0)
         Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4)
@@ -85,7 +117,7 @@ Partial Class Clientes
         'BtnRegresar
         '
         Me.BtnRegresar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnRegresar.Location = New System.Drawing.Point(8, 23)
+        Me.BtnRegresar.Location = New System.Drawing.Point(864, 91)
         Me.BtnRegresar.Margin = New System.Windows.Forms.Padding(4)
         Me.BtnRegresar.Name = "BtnRegresar"
         Me.BtnRegresar.Size = New System.Drawing.Size(164, 63)
@@ -104,28 +136,6 @@ Partial Class Clientes
         Me.BtnModificar.Text = "Modificar Cliente"
         Me.BtnModificar.UseVisualStyleBackColor = True
         '
-        'BtnBuscar
-        '
-        Me.BtnBuscar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnBuscar.Location = New System.Drawing.Point(525, 23)
-        Me.BtnBuscar.Margin = New System.Windows.Forms.Padding(4)
-        Me.BtnBuscar.Name = "BtnBuscar"
-        Me.BtnBuscar.Size = New System.Drawing.Size(164, 63)
-        Me.BtnBuscar.TabIndex = 2
-        Me.BtnBuscar.Text = "Buscar Cliente"
-        Me.BtnBuscar.UseVisualStyleBackColor = True
-        '
-        'BtnEliminar
-        '
-        Me.BtnEliminar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnEliminar.Location = New System.Drawing.Point(353, 23)
-        Me.BtnEliminar.Margin = New System.Windows.Forms.Padding(4)
-        Me.BtnEliminar.Name = "BtnEliminar"
-        Me.BtnEliminar.Size = New System.Drawing.Size(164, 63)
-        Me.BtnEliminar.TabIndex = 1
-        Me.BtnEliminar.Text = "Eliminar Cliente"
-        Me.BtnEliminar.UseVisualStyleBackColor = True
-        '
         'BtnIngresar
         '
         Me.BtnIngresar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -137,14 +147,25 @@ Partial Class Clientes
         Me.BtnIngresar.Text = "Ingresar Cliente"
         Me.BtnIngresar.UseVisualStyleBackColor = True
         '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEliminar.Location = New System.Drawing.Point(353, 23)
+        Me.BtnEliminar.Margin = New System.Windows.Forms.Padding(4)
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(172, 63)
+        Me.BtnEliminar.TabIndex = 1
+        Me.BtnEliminar.Text = "Eliminar Cliente"
+        Me.BtnEliminar.UseVisualStyleBackColor = True
+        '
         'Label1
         '
         Me.Label1.BackColor = System.Drawing.Color.Aquamarine
         Me.Label1.Font = New System.Drawing.Font("Arial", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(16, 11)
+        Me.Label1.Location = New System.Drawing.Point(13, 11)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(883, 53)
+        Me.Label1.Size = New System.Drawing.Size(1023, 53)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Gestionar Cliente"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -160,7 +181,7 @@ Partial Class Clientes
         Me.GroupBox3.Controls.Add(Me.TxtNomCli)
         Me.GroupBox3.Controls.Add(Me.Label2)
         Me.GroupBox3.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox3.Location = New System.Drawing.Point(16, 181)
+        Me.GroupBox3.Location = New System.Drawing.Point(16, 209)
         Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4)
@@ -256,7 +277,7 @@ Partial Class Clientes
         Me.GroupBox4.Controls.Add(Me.TxtNomEmpresa)
         Me.GroupBox4.Controls.Add(Me.Label7)
         Me.GroupBox4.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox4.Location = New System.Drawing.Point(20, 378)
+        Me.GroupBox4.Location = New System.Drawing.Point(16, 409)
         Me.GroupBox4.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox4.Name = "GroupBox4"
         Me.GroupBox4.Padding = New System.Windows.Forms.Padding(4)
@@ -341,22 +362,48 @@ Partial Class Clientes
         Me.Label7.TabIndex = 0
         Me.Label7.Text = "Nombre Empresa:"
         '
-        'DataGridView1
+        'Dgv_Listado
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(341, 230)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 51
-        Me.DataGridView1.Size = New System.Drawing.Size(561, 309)
-        Me.DataGridView1.TabIndex = 7
+        Me.Dgv_Listado.AllowUserToAddRows = False
+        Me.Dgv_Listado.AllowUserToDeleteRows = False
+        Me.Dgv_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Dgv_Listado.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.eliminar})
+        Me.Dgv_Listado.Location = New System.Drawing.Point(356, 238)
+        Me.Dgv_Listado.Margin = New System.Windows.Forms.Padding(4)
+        Me.Dgv_Listado.Name = "Dgv_Listado"
+        Me.Dgv_Listado.ReadOnly = True
+        Me.Dgv_Listado.RowHeadersWidth = 51
+        Me.Dgv_Listado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.Dgv_Listado.Size = New System.Drawing.Size(683, 366)
+        Me.Dgv_Listado.TabIndex = 7
+        '
+        'eliminar
+        '
+        Me.eliminar.HeaderText = "Eliminar"
+        Me.eliminar.MinimumWidth = 6
+        Me.eliminar.Name = "eliminar"
+        Me.eliminar.ReadOnly = True
+        Me.eliminar.Width = 125
+        '
+        'Chk_Eliminar
+        '
+        Me.Chk_Eliminar.AutoSize = True
+        Me.Chk_Eliminar.Location = New System.Drawing.Point(356, 209)
+        Me.Chk_Eliminar.Margin = New System.Windows.Forms.Padding(4)
+        Me.Chk_Eliminar.Name = "Chk_Eliminar"
+        Me.Chk_Eliminar.Size = New System.Drawing.Size(80, 21)
+        Me.Chk_Eliminar.TabIndex = 11
+        Me.Chk_Eliminar.Text = "Eliminar"
+        Me.Chk_Eliminar.UseVisualStyleBackColor = True
         '
         'Clientes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(915, 552)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.ClientSize = New System.Drawing.Size(1078, 617)
+        Me.Controls.Add(Me.BtnRegresar)
+        Me.Controls.Add(Me.Chk_Eliminar)
+        Me.Controls.Add(Me.Dgv_Listado)
         Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox1)
@@ -365,13 +412,14 @@ Partial Class Clientes
         Me.Name = "Clientes"
         Me.Text = "Clientes"
         Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
         Me.GroupBox3.PerformLayout()
         Me.GroupBox4.ResumeLayout(False)
         Me.GroupBox4.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Dgv_Listado, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -379,7 +427,6 @@ Partial Class Clientes
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents BtnRegresar As Button
     Friend WithEvents BtnModificar As Button
-    Friend WithEvents BtnBuscar As Button
     Friend WithEvents BtnEliminar As Button
     Friend WithEvents BtnIngresar As Button
     Friend WithEvents Label1 As Label
@@ -399,7 +446,12 @@ Partial Class Clientes
     Friend WithEvents Label6 As Label
     Friend WithEvents TxtNomEmpresa As TextBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents TxtIDCliente As TextBox
     Friend WithEvents Label9 As Label
+    Friend WithEvents Txt_Buscar As TextBox
+    Friend WithEvents Cmb_Buscar As ComboBox
+    Friend WithEvents Lb_Buscar As Label
+    Friend WithEvents Dgv_Listado As DataGridView
+    Friend WithEvents eliminar As DataGridViewCheckBoxColumn
+    Friend WithEvents Chk_Eliminar As CheckBox
 End Class
--- CRM/Presentacion/Clientes.vb
+++ CRM/Presentacion/Clientes.vb
@@ -1,25 +1,64 @@
 ﻿Public Class Clientes
+
     Private TablaDatos As New DataTable
+    Public Bandera As New Boolean
     Private Sub Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Mostrar()
+        Cmb_Buscar.Items.Add("Cli_Nombre")
+        Cmb_Buscar.Items.Add("Cli_Telefono")
+        Cmb_Buscar.Items.Add("Cli_Email")
+        Cmb_Buscar.Text = "Cli_Nombre"
         TxtIDCliente.Enabled = False
 
+        If Bandera Then
+            BtnRegresar.Visible = False
+        Else
+            BtnRegresar.Visible = True
+        End If
+        Mostrar()
+        Limpiar()
     End Sub
 
     Private Sub Mostrar()
         Try
             Dim Funcion As New fClientes
             TablaDatos = Funcion.Mostrar
+            Dgv_Listado.Columns.Item("eliminar").Visible = False
             If TablaDatos.Rows.Count <> 0 Then
-                DataGridView1.DataSource = TablaDatos
-                DataGridView1.ColumnHeadersVisible = True
+                Dgv_Listado.DataSource = TablaDatos
+                Dgv_Listado.ColumnHeadersVisible = True
             Else
-                DataGridView1.DataSource = Nothing
-                DataGridView1.ColumnHeadersVisible = False
+                Dgv_Listado.DataSource = Nothing
+                Dgv_Listado.ColumnHeadersVisible = False
             End If
         Catch Evento As Exception
             MsgBox(Evento.Message)
         End Try
+        Buscar()
+    End Sub
+
+    Private Sub Buscar()
+        Try
+            Dim ConjuntoDatos As New DataSet
+            ConjuntoDatos.Tables.Add(TablaDatos.Copy)
+            Dim VistaDatos As New DataView(ConjuntoDatos.Tables(0))
+            VistaDatos.RowFilter = Cmb_Buscar.Text & " like '" & Txt_Buscar.Text & "%'"
+            If VistaDatos.Count <> 0 Then
+                Dgv_Listado.DataSource = VistaDatos
+                OcultarColumna()
+            Else
+                Dgv_Listado.DataSource = Nothing
+            End If
+        Catch Evento As Exception
+            MsgBox(Evento.Message)
+        End Try
+    End Sub
+
+    Private Sub OcultarColumna()
+        Dgv_Listado.Columns(1).Visible = False
+    End Sub
+
+    Private Sub Txt_Buscar_TextChanged(sender As Object, e As EventArgs) Handles Txt_Buscar.TextChanged
+        Buscar()
     End Sub
 
     Private Sub Limpiar()
@@ -31,27 +70,37 @@
         TxtTelEmpresa.Text = ""
         TxtEmailEmpresa.Text = ""
         TxtRTN.Text = ""
+
+        BtnModificar.Visible = False
+
     End Sub
 
-    'Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
-    '  TrasladoInformacion()
-    'End Sub
-    ' Private Sub TrasladoInformacion()
-    '   TxtIDCliente.Text = DataGridView1.SelectedCells.Item(0).Value
-    '  TxtNomCli.Text = DataGridView1.SelectedCells.Item(1).Value
-    ' TxtTelCli.Text = DataGridView1.SelectedCells.Item(2).Value
-    'TxtEmailCli.Text = DataGridView1.SelectedCells.Item(3).Value
-    'TxtNomEmpresa.Text = DataGridView1.SelectedCells.Item(4).Value
-    'TxtTelEmpresa.Text = DataGridView1.SelectedCells.Item(5).Value
-    'TxtEmailEmpresa.Text = DataGridView1.SelectedCells.Item(6).Value
-    'TxtRTN.Text = DataGridView1.SelectedCells.Item(7).Value
+    Private Sub Dgv_Listado_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Listado.CellClick
+        TrasladoInformacion()
+        If Bandera Then
+            BtnModificar.Visible = False
+        Else
+            BtnModificar.Visible = True
+        End If
 
-    'End Sub
+    End Sub
+
+    Private Sub TrasladoInformacion()
+        TxtIDCliente.Text = Dgv_Listado.SelectedCells.Item(1).Value
+        TxtNomCli.Text = Dgv_Listado.SelectedCells.Item(2).Value
+        TxtTelCli.Text = Dgv_Listado.SelectedCells.Item(3).Value
+        TxtEmailCli.Text = Dgv_Listado.SelectedCells.Item(4).Value
+        TxtNomEmpresa.Text = Dgv_Listado.SelectedCells.Item(5).Value
+        TxtTelEmpresa.Text = Dgv_Listado.SelectedCells.Item(6).Value
+        TxtEmailEmpresa.Text = Dgv_Listado.SelectedCells.Item(7).Value
+        TxtRTN.Text = Dgv_Listado.SelectedCells.Item(8).Value
+    End Sub
 
     Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles BtnIngresar.Click
         If TxtNomCli.Text <> "" And TxtTelCli.Text <> "" And TxtEmailCli.Text <> "" And
             TxtNomEmpresa.Text <> "" And TxtTelEmpresa.Text <> "" And TxtEmailEmpresa.Text <> "" And
             TxtRTN.Text <> "" Then
+
             Try
                 Dim TablaDatos As New eClientes
                 Dim Funcion As New fClientes
@@ -84,12 +133,114 @@
         End If
     End Sub
 
+    Private Sub BtnModificar_Click(sender As Object, e As EventArgs) Handles BtnModificar.Click
+        If TxtNomCli.Text <> "" And TxtTelCli.Text <> "" And TxtEmailCli.Text <> "" And
+            TxtNomEmpresa.Text <> "" And TxtTelEmpresa.Text <> "" And TxtEmailEmpresa.Text <> "" And
+            TxtRTN.Text <> "" Then
+
+            Dim Resultado As DialogResult
+            Resultado = MessageBox.Show("Desea Modificar los datos",
+            "Actualizando Registro", MessageBoxButtons.OKCancel,
+            MessageBoxIcon.Question)
+            If Resultado = Windows.Forms.DialogResult.OK Then
+                Try
+                    Dim TablaDatos As New eClientes
+                    Dim Funcion As New fClientes
+                    TablaDatos.pID_Cliente = TxtIDCliente.Text
+                    TablaDatos.pCli_Nombre = TxtNomCli.Text
+                    TablaDatos.pCli_Telefono = TxtTelCli.Text
+                    TablaDatos.pCli_Email = TxtEmailCli.Text
+                    TablaDatos.pCli_NombreEmpresa = TxtNomEmpresa.Text
+                    TablaDatos.pCli_TelEmpresa = TxtTelEmpresa.Text
+                    TablaDatos.pCli_EmailEmpresa = TxtEmailEmpresa.Text
+                    TablaDatos.pRTN = TxtRTN.Text
+                    If Funcion.Actualizar(TablaDatos) Then
+                        MessageBox.Show("Cliente fue actualizado correctamente",
+                     "Actualizando Registro", MessageBoxButtons.OK,
+                      MessageBoxIcon.Information)
+                    Else
+                        MessageBox.Show("Cliente no fue actualizado correctamente",
+                     "Actualizando Registro", MessageBoxButtons.OK,
+                      MessageBoxIcon.Information)
+                    End If
+                    Mostrar()
+                    Limpiar()
+                Catch Evento As Exception
+                    MsgBox(Evento.Message)
+                End Try
+            Else
+                MessageBox.Show("Cancelado por el Usuario",
+                      "Guardando Registro", MessageBoxButtons.OK,
+                       MessageBoxIcon.Information)
+            End If
+        Else
+            MessageBox.Show("Falta Informacion para almacenar",
+                      "Guardando Registro", MessageBoxButtons.OK,
+                       MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub Chk_Eliminar_CheckedChanged(sender As Object, e As EventArgs) Handles Chk_Eliminar.CheckedChanged
+        If Chk_Eliminar.CheckState = CheckState.Unchecked Then
+            Dgv_Listado.Columns.Item("Eliminar").Visible = False
+            BtnEliminar.Visible = False
+
+        Else
+            Dgv_Listado.Columns.Item("Eliminar").Visible = True
+            BtnEliminar.Visible = True
+        End If
+    End Sub
+
+    Private Sub Dgv_Listado_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Listado.CellContentClick
+        If e.ColumnIndex = Me.Dgv_Listado.Columns.Item("Eliminar").Index Then
+            Dim ChkCell As DataGridViewCheckBoxCell = Me.Dgv_Listado.Rows(e.RowIndex).Cells("Eliminar")
+            ChkCell.Value = Not ChkCell.Value
+        End If
+    End Sub
+
+    Private Sub BtnEliminar_Click(sender As Object, e As EventArgs) Handles BtnEliminar.Click
+        Dim Resultado As DialogResult
+        Resultado = MessageBox.Show("Desea Eliminar los datos",
+        "Eliminando Registro", MessageBoxButtons.OKCancel,
+        MessageBoxIcon.Question)
+        If Resultado = Windows.Forms.DialogResult.OK Then
+            Try
+                For Each row As DataGridViewRow In Dgv_Listado.Rows
+                    Dim LineaMarca As Boolean = Convert.ToBoolean(row.Cells("Eliminar").Value)
+                    If LineaMarca Then
+                        Dim LlavePrimaria As Integer = Convert.ToInt32(row.Cells("ID_Cliente").Value)
+                        Dim TablaDatos As New eClientes
+                        Dim Funcion As New fClientes
+                        TablaDatos.pID_Cliente = LlavePrimaria
+                        If Funcion.Eliminar(TablaDatos) Then
+                            MessageBox.Show("Cliente fue eliminado correctamente",
+                    "Eliminando Registro", MessageBoxButtons.OK,
+                     MessageBoxIcon.Information)
+                        Else
+                            MessageBox.Show("Cancelado por el Usuario",
+                    "Guardando Registro", MessageBoxButtons.OK,
+                     MessageBoxIcon.Information)
+                        End If
+                    End If
+                Next
+                Call Mostrar()
+                Call Limpiar()
+            Catch Evento As Exception
+                MsgBox(Evento.Message)
+            End Try
+        Else
+            MessageBox.Show("Cancelado por el Usuario",
+                    "Guardando Registro", MessageBoxButtons.OK,
+                     MessageBoxIcon.Information)
+            Call Mostrar()
+            Call Limpiar()
+        End If
+    End Sub
 
     Private Sub BtnRegresar_Click(sender As Object, e As EventArgs) Handles BtnRegresar.Click
         Inicio.Visible = True
         Me.Close()
     End Sub
-
 
 
 End Class

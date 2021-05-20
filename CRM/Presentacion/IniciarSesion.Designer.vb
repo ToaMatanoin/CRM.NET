0da392e49be92d9fff1ec868269f3a135ad95733
Julio Alejandro Rodríguez Medina <jarodriguezm2000@gmail.com>
2021-05-20 14:04:49 -0600

    imagen

--- CRM/Presentacion/IniciarSesion.Designer.vb
+++ CRM/Presentacion/IniciarSesion.Designer.vb
@@ -22,6 +22,7 @@ Partial Class IniciarSesion
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(IniciarSesion))
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Txt_UsuPass = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -29,26 +30,29 @@ Partial Class IniciarSesion
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Inicio_Sesion = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
         Me.GroupBox1.Controls.Add(Me.Txt_UsuPass)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Txt_UsuNom)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.Button2)
         Me.GroupBox1.Controls.Add(Me.Inicio_Sesion)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(224, 116)
+        Me.GroupBox1.Size = New System.Drawing.Size(224, 348)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         '
         'Txt_UsuPass
         '
-        Me.Txt_UsuPass.Location = New System.Drawing.Point(106, 46)
+        Me.Txt_UsuPass.Location = New System.Drawing.Point(102, 272)
         Me.Txt_UsuPass.Name = "Txt_UsuPass"
         Me.Txt_UsuPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.Txt_UsuPass.Size = New System.Drawing.Size(100, 20)
@@ -58,7 +62,7 @@ Partial Class IniciarSesion
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(34, 47)
+        Me.Label2.Location = New System.Drawing.Point(19, 273)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(66, 16)
         Me.Label2.TabIndex = 4
@@ -66,7 +70,7 @@ Partial Class IniciarSesion
         '
         'Txt_UsuNom
         '
-        Me.Txt_UsuNom.Location = New System.Drawing.Point(106, 20)
+        Me.Txt_UsuNom.Location = New System.Drawing.Point(102, 246)
         Me.Txt_UsuNom.Name = "Txt_UsuNom"
         Me.Txt_UsuNom.Size = New System.Drawing.Size(100, 20)
         Me.Txt_UsuNom.TabIndex = 3
@@ -75,7 +79,7 @@ Partial Class IniciarSesion
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(34, 21)
+        Me.Label1.Location = New System.Drawing.Point(19, 247)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(48, 16)
         Me.Label1.TabIndex = 2
@@ -83,7 +87,7 @@ Partial Class IniciarSesion
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(131, 82)
+        Me.Button2.Location = New System.Drawing.Point(143, 310)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 1
@@ -92,23 +96,34 @@ Partial Class IniciarSesion
         '
         'Inicio_Sesion
         '
-        Me.Inicio_Sesion.Location = New System.Drawing.Point(12, 82)
+        Me.Inicio_Sesion.Location = New System.Drawing.Point(8, 310)
         Me.Inicio_Sesion.Name = "Inicio_Sesion"
         Me.Inicio_Sesion.Size = New System.Drawing.Size(99, 23)
         Me.Inicio_Sesion.TabIndex = 0
         Me.Inicio_Sesion.Text = "Iniciar Sesion"
         Me.Inicio_Sesion.UseVisualStyleBackColor = True
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 19)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(210, 211)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
         'IniciarSesion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(250, 136)
+        Me.ClientSize = New System.Drawing.Size(250, 372)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "IniciarSesion"
         Me.Text = "Iniciar Sesion"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -120,4 +135,5 @@ Partial Class IniciarSesion
     Friend WithEvents Label1 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents Inicio_Sesion As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class

25015254626e506ea960f22fd25738b61e4c6d28
Julio Alejandro Rodríguez Medina <jarodriguezm2000@gmail.com>
2021-06-08 20:01:01 -0600

    Cambios 08/06/21 20:00

--- CRM/Presentacion/IniciarSesion.vb
+++ CRM/Presentacion/IniciarSesion.vb
@@ -11,7 +11,7 @@
         Me.Close()
     End Sub
 
-    Private Sub Inicio_Sesion_Click(sender As Object, e As EventArgs) Handles Inicio_Sesion.Click
+    Private Sub Metodo()
         Try
             If (Txt_UsuNom.Text.Equals("")) Then
                 MsgBox("Ingresa un usuario")
@@ -44,76 +44,17 @@
         End Try
     End Sub
 
-    Private Sub Password_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Txt_UsuPass.KeyDown
-        Select Case e.KeyData
-            Case Keys.Enter
-                Try
-                    If (Txt_UsuNom.Text.Equals("")) Then
-                        MsgBox("Ingresa un usuario")
-                    Else
 
-                        If nuevo.Usuario(Txt_UsuNom.Text) = True Then
-                            Dim contra As String = nuevo.Contrasenha(Txt_UsuNom.Text)
-                            If (Txt_UsuPass.Text.Equals("")) Then
-                                MsgBox("Ingresa contrasea")
-                            Else
-                                If contra.Equals(Txt_UsuPass.Text) Then
-                                    nuevo.ConexionDB()
-                                    IDUSU = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "ID_Usuario", "Usuarios")
-                                    IDEMP = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "ID_Empleado", "Usuarios")
-                                    ROLUSU = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "Usu_Cargo", "Usuarios")
-                                    Me.Hide()
-                                    Txt_UsuNom.Text = ""
-                                    Txt_UsuPass.Text = ""
-                                    Inicio.Show()
-                                Else
-                                    MsgBox("Contraseña Invalida", MsgBoxStyle.Critical)
-                                End If
-                            End If
-                        Else
-                            MsgBox("El Usuario: " + Txt_UsuNom.Text + " no se encuentra registrado")
-                        End If
-                    End If
-                Catch ex As Exception
-                    MsgBox(ex.ToString)
-                End Try
-        End Select
+    Private Sub Inicio_Sesion_Click(sender As Object, e As EventArgs) Handles Inicio_Sesion.Click
+        Metodo()
+    End Sub
+
+    Private Sub Password_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Txt_UsuPass.KeyDown
+        Metodo()
     End Sub
 
     Private Sub Usuario_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Txt_UsuNom.KeyDown
-        Select Case e.KeyData
-            Case Keys.Enter
-                Try
-                    If (Txt_UsuNom.Text.Equals("")) Then
-                        MsgBox("Ingresa un usuario")
-                    Else
-
-                        If nuevo.Usuario(Txt_UsuNom.Text) = True Then
-                            Dim contra As String = nuevo.Contrasenha(Txt_UsuNom.Text)
-                            If (Txt_UsuPass.Text.Equals("")) Then
-                                MsgBox("Ingresa contrasea")
-                            Else
-                                If contra.Equals(Txt_UsuPass.Text) Then
-                                    nuevo.ConexionDB()
-                                    IDUSU = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "ID_Usuario", "Usuarios")
-                                    IDEMP = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "ID_Empleado", "Usuarios")
-                                    ROLUSU = nuevo.Buscar_info(Txt_UsuNom.Text, "Usu_Nombre", "Usu_Cargo", "Usuarios")
-                                    Me.Hide()
-                                    Txt_UsuNom.Text = ""
-                                    Txt_UsuPass.Text = ""
-                                    Inicio.Show()
-                                Else
-                                    MsgBox("Contraseña Invalida", MsgBoxStyle.Critical)
-                                End If
-                            End If
-                        Else
-                            MsgBox("El Usuario: " + Txt_UsuNom.Text + " no se encuentra registrado")
-                        End If
-                    End If
-                Catch ex As Exception
-                    MsgBox(ex.ToString)
-                End Try
-        End Select
+        Metodo()
     End Sub
 
 End Class
